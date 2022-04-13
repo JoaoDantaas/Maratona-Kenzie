b13ae0fdbf42835a9ccf68f835675bdb4ee4c605
JoaoDantaas <joao.araujosp@gmail.com>
2022-04-12 22:56:59 -0300

    Maratona Kenzie

---
+++ index.js
@@ -0,0 +1,19 @@
+let competidores = ["Daniel","Rafel","Manoel"]
+
+function positions(firstPlace, secondPlace, lastPlace){
+  if (firstPlace == "Daniel"){
+    console.log("1ºLugar Daniel")
+    console.log("2ºLugar Rafael")
+    console.log("3ºLugar Manoel")
+  }else if(secondPlace == "Daniel"){
+    console.log("1ºLugar Daniel")
+    console.log("2ºLugar Rafael")
+    console.log("3ºLugar Manoel")
+  }else(lastPlace == "Daniel")
+    console.log("1ºLugar Daniel")
+    console.log("2ºLugar Manoel")
+    console.log("3ºLugar Rafael")
+}
+
+
+positions(competidores)
